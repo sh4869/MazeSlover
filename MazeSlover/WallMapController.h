02e66f25120ad66a8a52172769d69dea4548103b
sh4869 <nobuk4869@gmail.com>
2017-10-10 18:47:23 +0900

    Remove WallTypes.h

--- MazeSlover/WallMapController.h
+++ MazeSlover/WallMapController.h
@@ -1,8 +1,27 @@
 #ifndef MAPCONTROLLER_H_
 #define MAPCONTROLLER_H_
 
-#include "WallTypes.h"
 #include <array>
+#include <bitset>
+#include <utility>
+
+// Types
+enum class PosStatus : char { UNSEARCHED, SEARCHED, GOAL, CURRENT };
+// 4bitで上の桁から下左だけを保存
+using WallInfo = std::pair<std::bitset<8>, PosStatus>;
+static constexpr char mazeSize = 16;
+// X,Yの順番で指定することにします
+using WallMap = std::array<std::array<WallInfo, mazeSize>, mazeSize>;
+
+using MapPosition = std::pair<char, char>;
+// Function Alias
+static constexpr auto mapPos = std::make_pair<MapPosition::first_type, MapPosition::second_type>;
+
+using StepMap = std::array<std::array<unsigned char, mazeSize>, mazeSize>;
+
+static constexpr char goalSize = 1;
+using GoalPositon = std::array<MapPosition, goalSize>;
+
 
 enum class MapDirection : int { LEFT, BACK, RIGHT, FRONT };
 
